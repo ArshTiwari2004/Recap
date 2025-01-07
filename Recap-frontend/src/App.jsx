5904c2cc1ad7de9014fbaf8ed50c130677a5e804
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-01-08 01:57:39 +0530

    Premiumsubscription

--- Recap-frontend/src/App.jsx
+++ Recap-frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import LandingPage from './pages/Landingpage'
+import Premium from './pages/Premium'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/premium" element={<Premium />} />
         </Routes>
     </Router>
   )
