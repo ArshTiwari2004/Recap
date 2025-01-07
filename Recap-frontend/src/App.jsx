72de34a83ec9318943f2cc02f399b2d8ba7d3e33
Nibedan <nibedanpati@gmail.com>
2025-01-08 03:50:28 +0530

    Github Authentication Added

--- Recap-frontend/src/App.jsx
+++ Recap-frontend/src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import LandingPage from './pages/Landingpage'
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
+    <>
+    <Toaster />
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         </Routes>
     </Router>
+    </>
   )
 }
 
